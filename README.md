9f177ab3d982841dcb835364fcf4ca5e3ee3af09
nethe550 <nethermaster5505@gmail.com>
2023-10-18 13:33:49 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tont-doom
+doom tont
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import view.Window;
+
+public class Main {
+
+    private static final Window window = new Window();
+
+    public static void main(String[] args) {
+        window.run();
+    }
+
+}
